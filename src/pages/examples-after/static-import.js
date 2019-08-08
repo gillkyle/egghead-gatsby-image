55ffcd82f6c56a99de53a4d5dcfad41f73daee13
Kyle Gill <kylerobertgill@gmail.com>
2019-08-08 10:10:21 -0600

    fix static import

--- src/pages/examples-after/static-import.js
+++ src/pages/examples-after/static-import.js
@@ -10,7 +10,7 @@ import Layout from "../../components/layout"
 export default () => {
   return (
     <Layout>
-      <img src={`corgi.jpg`} />
+      <img src={`../../corgi.jpg`} />
     </Layout>
   )
 }
