486b7df7b7712599d046208be909bc131cbeff66
Kyle Gill <kylerobertgill@gmail.com>
2019-08-13 13:58:34 -0600

    updates for clarity

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,33 @@ import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
+const extractName = path => {
+  const name = path.split("/")[2]
+  return name
+}
+
 const IndexPage = ({
   data: {
     allSitePage: { nodes: pages },
   },
 }) => (
   <Layout>
+    <h1>Examples</h1>
     {pages.map((page, index) => {
       return (
-        <>
-          <Link to={page.path}>{page.path}</Link>
+        <div style={{ marginBottom: 20 }}>
+          <Link
+            style={{
+              border: "1px solid #CCC",
+              borderRadius: 4,
+              padding: 10,
+            }}
+            to={page.path}
+          >
+            {extractName(page.path)}
+          </Link>
           <br />
-        </>
+        </div>
       )
     })}
   </Layout>
@@ -24,7 +39,7 @@ export default IndexPage
 
 export const query = graphql`
   query HomePageQuery {
-    allSitePage(filter: { path: { regex: "/examples-after/" } }) {
+    allSitePage(filter: { path: { regex: "/examples/" } }) {
       nodes {
         path
       }
